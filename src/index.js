937e42157d0326290d731395f0b37d1627fd2e71
infantkeu <70271844+infantkeu@users.noreply.github.com>
2020-08-26 17:29:06 +0300

    1stt component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+//import App from "./App";
+
+function MovieItem(props) {
+  console.log("props=", props);
+  return (
+    <div>
+      <p>{props.title}</p>
+      <p>{props.rate}</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <MovieItem title="title_new" rate={10.1} />
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
