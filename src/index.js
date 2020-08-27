5551589078fe52d96d5080d5a7167393d65365c4
infantkeu <70271844+infantkeu@users.noreply.github.com>
2020-08-27 16:58:02 +0300

    but2

--- src/index.js
+++ src/index.js
@@ -6,27 +6,59 @@ import ReactDOM from "react-dom";
 const movie = {
   title: "Avarage",
   rate: "10.1",
-  image: "https://rwhi6.csb.app/avarage.jpg",
+  image:
+    "https://uploads.codesandbox.io/uploads/user/5f703e8f-97ef-4847-a9eb-2fa75408766c/p3t1-average.jpg",
   overview: "Описание фильма!!!!"
 };
 
-function Image (props) {
-  console.log("Image props", props);
-  return (
-    <img src={props.src} alt={props.alt} />
-  );
+function Image(props) {
+  //  console.log("Image props", props);
+  return <img src={props.src} alt={props.alt} />;
 }
 
-function MovieItem (props) {
-  console.log("props=", props);
-  const { data: { title, rate, image } } = props;
-  return (
-    <div>
-      <Image src={image} alt={title} />
-      <p>{title}</p>
-      <p>{rate}</p>
-    </div>
-  );
+//function MovieItem (props) {
+//  console.log("props=", props);
+//  const { data: { title, rate, image } } = props;
+//  return (
+//    <div>
+//      <Image src={image} alt={title} />
+//      <p>{title}</p>
+//      <p>{rate}</p>
+//    </div>
+//  );
+//}
+
+class MovieItem extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      show: false
+    };
+  }
+  render() {
+    console.log(this.state);
+    const {
+      data: { title, rate, image, overview }
+    } = this.props;
+    return (
+      <div>
+        <Image src={image} alt={title} />
+        <p>{title}</p>
+        <p>{rate}</p>
+        <button
+          type="button"
+          onClick={() => {
+            console.log("show");
+            this.setState({ show: true });
+          }}
+        >
+          {this.state.show === true ? <p>hide</p> : <p>show</p>}
+        </button>
+        {this.state.show === true ? <p>{overview}</p> : null}
+      </div>
+    );
+  }
 }
 
 function App() {
