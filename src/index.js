030bf9c771b740ffc612535c29978cafa5a5baaf
infantkeu <70271844+infantkeu@users.noreply.github.com>
2020-08-28 17:46:40 +0300

    styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
 
 //import App from "./App";
 
@@ -7,13 +8,13 @@ const movie = {
   title: "Avarage",
   rate: "10.1",
   image:
-    "https://uploads.codesandbox.io/uploads/user/5f703e8f-97ef-4847-a9eb-2fa75408766c/p3t1-average.jpg",
+    "https://uploads.codesandbox.io/uploads/user/5f703e8f-97ef-4847-a9eb-2fa75408766c/p3t1-average.jpg    ",
   overview: "Описание фильма!!!!"
 };
 
 function Image(props) {
   //  console.log("Image props", props);
-  return <img src={props.src} alt={props.alt} />;
+  return <img src={props.src} alt={props.alt} width="100%" />;
 }
 
 //function MovieItem (props) {
@@ -33,38 +34,43 @@ class MovieItem extends React.Component {
     super();
 
     this.state = {
-      show: false
+      show: false,
+      like: false
     };
   }
+
+  toggleOverview = () => {
+    console.log("show");
+    this.setState({ show: !this.state.show });
+  };
+
+  handleLike = () => {
+    this.setState({ like: !this.state.like });
+  };
+
   render() {
     console.log(this.state);
     const {
       data: { title, rate, image, overview }
     } = this.props;
     return (
-      <div>
+      <div style={{ width: "300px" }}>
         <Image src={image} alt={title} />
         <p>{title}</p>
         <p>{rate}</p>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("show");
-            this.setState({ show: true });
-          }}
-        >
-          show
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("show");
-            this.setState({ show: false });
-          }}
-        >
-          hide
-        </button>
-        {this.state.show === true ? <p>{overview}</p> : null}
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <button type="button" onClick={this.toggleOverview}>
+            {this.state.show ? "hide" : "show"}
+          </button>
+          <button
+            type="button"
+            onClick={this.handleLike}
+            className={this.state.like ? "btnLike" : ""}
+          >
+            like
+          </button>
+        </div>
+        {this.state.show ? <p>{overview}</p> : null}
       </div>
     );
   }
